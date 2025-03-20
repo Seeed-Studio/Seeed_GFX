c8b38a00dae6bbbd58b65d16eea4edafc93125e6
StarSphere <starsphere@foxmail.com>
2025-03-20 14:25:14 +0800

    feat: Add support for Seeed XIAO EPaper Displays
    Add User_Setups for Seeed XIAO EPaper Displays
    
    Add driver for SSD1680 and SSD1681 controllers
    
    Fixed the UC8179 driver naming error
    
    Fixd a bug in the EPaper library that caused the display to be inverted
    
    Resolve EPaper compilation compatibility issues
    
    Fixed ESP32C3 DMA crash issue

--- examples/ePaper/Basic/Clock/Clock.ino
+++ examples/ePaper/Basic/Clock/Clock.ino
@@ -18,8 +18,9 @@
  */
 
 #include <SPI.h>
-#define SEEED_XIAO_EPAPER_7INCH5
 #include <TFT_eSPI.h> // Hardware-specific library
+
+#ifdef EPAPER_ENABLE // Only compile this code if the EPAPER_ENABLE is defined in User_Setup.h
 
 EPaper epaper = EPaper(); // Invoke custom library
 
@@ -154,3 +155,4 @@ static uint8_t conv2d(const char *p)
     v = *p - '0';
   return 10 * v + *++p - '0';
 }
+#endif
