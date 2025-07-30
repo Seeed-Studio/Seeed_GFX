f18e39e16f33c3722fa71c1b4fb26690189ba12a
xcjk-tofuture <xcjk2024@gmail.com>
2025-07-30 11:02:48 +0800

    refactor(epaper): restore the TFT 6-color support

--- examples/ePaper/Colorful/HelloWorld/HelloWorld.ino
+++ examples/ePaper/Colorful/HelloWorld/HelloWorld.ino
@@ -1,3 +1,14 @@
+/*This is a 6-color electronic ink screen, which can only display 6 colors. 
+Any other colors will be mapped to similar ones.
+
+Here is the 6 colors you can display:
+1.TFT_WHITE
+2.TFT_BLACK
+3.TFT_YELLOW
+4.TFT_GREEN
+5.TFT_BLUE
+6.TFT_RED
+*/
 #include "TFT_eSPI.h"
 
 #ifdef EPAPER_ENABLE // Only compile this code if the EPAPER_ENABLE is defined in User_Setup.h
@@ -8,16 +19,16 @@ void setup()
 {
 #ifdef EPAPER_ENABLE
     epaper.begin();
-    epaper.fillScreen(EPAPER_WHITE);
+    epaper.fillScreen(TFT_WHITE);
 
-    epaper.fillCircle(25, 25, 15, EPAPER_RED);
-    epaper.fillRect(epaper.width() - 40,  10, 30, 30, EPAPER_GREEN);
+    epaper.fillCircle(25, 25, 15, TFT_RED);
+    epaper.fillRect(epaper.width() - 40,  10, 30, 30, TFT_GREEN);
     
     for (int i = 0; i < epaper.height() / 80; i++)
     {
-        epaper.setTextColor(EPAPER_BLUE);
+        epaper.setTextColor(TFT_BLUE);
         epaper.setTextSize(i + 1);
-        epaper.drawLine(10, 70 + 60 * i, epaper.width() - 10, 70 + 60 * i, EPAPER_BLACK);
+        epaper.drawLine(10, 70 + 60 * i, epaper.width() - 10, 70 + 60 * i, TFT_BLACK);
         epaper.drawString("Hello ePaper", 10, 80 + 60 * i);
     }
     
