903813e051cb02dd7bd1d7a775be751cfd13d937
xcjk <2049368121@qq.com>
2025-12-04 11:14:18 +0800

    refactor: optimized the 4L grayscale demo.

--- examples/ePaper/Gray/Shape/Shape.ino
+++ examples/ePaper/Gray/Shape/Shape.ino
@@ -1,5 +1,15 @@
-#include "TFT_eSPI.h"
+/*This is a 4-color electronic ink screen, which can only display 4 colors. 
 
+Here is the 4 colors you can display:
+1.TFT_GRAY_0    black
+2.TFT_GRAY_1      |
+3.TFT_GRAY_2      |
+4.TFT_GRAY_3    white
+
+*/
+
+#include "TFT_eSPI.h"
+#include "image.h"
 #ifdef EPAPER_ENABLE  // Only compile this code if the EPAPER_ENABLE is defined in User_Setup.h
 EPaper epaper;
 #endif
@@ -20,35 +30,8 @@ void setup()
   epaper.update();
 
   epaper.fillScreen(TFT_GRAY_3);
-  for (int i = 0; i < epaper.height() / 80; i++)
-  {
-    epaper.setTextSize(i + 1);
-    epaper.drawString("Hello ePaper", 10, 80 + 60 * i);
-    epaper.update(); // update the display
-  }
-
-  // random rectangles
-  for (int i = 0; i < 10; i++)
-  {
-    epaper.fillRect(random(epaper.width()), random(epaper.height()), random(30), random(30), TFT_GRAY_2);
-    epaper.update(); // update the display
-  }
-
-  // random circles
-  for (int i = 0; i < 10; i++)
-  {
-    epaper.fillCircle(random(epaper.width()), random(epaper.height()), random(30), TFT_GRAY_1);
-    epaper.update(); // update the display
-  }
-
-  delay(2000);
-
-  for (int i = 0; i < epaper.width() / 80; i++)
-  {
-    // automatically wakes up, but you need to sleep again after drawing
-    epaper.fillRect(10 + 80 * i, 10, 40, 40, TFT_GRAY_2);
-    epaper.update();
-  }
+  epaper.pushImage(0, 0, 800, 480, (uint16_t *)L4_GRAY);
+  epaper.update();
 #endif
 }
 
